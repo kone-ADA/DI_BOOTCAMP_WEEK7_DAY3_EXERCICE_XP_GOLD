5f079144edf6a4a334b4d7b30ae34e4fd32e979d
kone-yaya-dev <64926396+kone-yaya-dev@users.noreply.github.com>
2023-02-13 12:59:53 +0000

    first commit

---
+++ Exercise_1_PHP_Login_With_Session/index.php
@@ -0,0 +1,35 @@
+<?php
+  session_start();
+
+
+  $msg = '';
+  if (isset($_POST['login'])) {
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+
+      if ($username == 'admin' && $password == 'password') {
+          $_SESSION['username'] = $username;
+          //header("Location: exercice1.php");
+          $msg = 'valid username ';
+      } else {
+          $msg = 'Invalid username or password';
+      }
+  }
+  ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login Form</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <h2><?php echo $msg; ?></h2>
+        <input type="text" name="username" placeholder="username" required />
+        <input type="password" name="password" placeholder="password" required />
+        <button type="submit" name="login">Login</button>
+    </form>
+</body>
+
+</html>
